7a0fc2676d2f894df0cc8259354899182712f4a2
Bhanuprakash <bhanuprakash.rb@sonata-software.com>
2018-07-11 10:14:54 +0530

    Enquiry form
    Enquiry form json conversions

--- js/Commonscript.js
+++ js/Commonscript.js
@@ -223,4 +223,30 @@ $('.as-accordion-head').click(function () {
 	});
 });
 
-$('.input-group.date').datepicker({format: "dd.mm.yyyy"}); 
+//$('.input-group.date').datepicker({format: "dd.mm.yyyy"}); 
+
+
+
+
+$.fn.serializeObject = function()
+{
+	var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+$(function() {
+    $('form').submit(function() {
+        $('#womenGarmentResult').text(JSON.stringify($('form').serializeObject()));
+        return false;
+    });
+});
